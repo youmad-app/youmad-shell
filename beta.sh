3ed5cfefb25959b3a00c8e10180ec8433310311a
skrodahl <38037686+skrodahl@users.noreply.github.com>
2025-07-16 16:33:34 +0200

    Update beta.sh

--- beta.sh
+++ beta.sh
@@ -1,1 +1,10 @@
 no beta currently available.
+
+TODO for next version:
+ - Convert WebM → Opus, without reencoding (ffmpeg -i input.webm -c:a copy output.opus)
+   - Consider if this should be the default mode, as it's bitperfect downloads.
+   - Add metadata (including custom fields), using ffmpeg
+   - Embed cover art
+ - Remove WebM as supported format, and clean up some cluttered logic
+ - Keep exiftool for all other formats, avoid retesting stuff that works
+   - ffmpeg is already a requirement for yt-dlp
